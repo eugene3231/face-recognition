3224fe6d3c51c2418d844e57ae35c1cb32a967be
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-27 15:46:19 +0800

    add boundingbox

--- src/App.js
+++ src/App.js
@@ -45,45 +45,60 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
   }
   
+  calculateFaceLocation = (data) => {
+    const faceDetected = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    console.log(width, height);
+    return {
+      leftCol: faceDetected.left_col * width,
+      topRow: faceDetected.top_row * height,
+      rightCol: width - (faceDetected.right_col * width),
+      bottomRow: height - (faceDetected.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+    }
+        
+  render() {
+    return (
+      <div className="App">
+      <Particles className='particles'
+      params={particleOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm 
+      onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}
+      />
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
       );
-      
     }
-    
-    onInputChange = (event) => {
-      this.setState({input: event.target.value});
-    }
-    
-    render() {
-      return (
-        <div className="App">
-        <Particles className='particles'
-        params={particleOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-        onInputChange={this.onInputChange} 
-        onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
-        </div>
-        );
-      }
-    }
+  }
     
     export default App;
     
