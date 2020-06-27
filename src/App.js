e6f695de185dbd5c4b8863426c5e6cec7aceb582
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-06-27 13:06:31 +0800

    first commit - aesthetics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+import 'tachyons';
+
+const particleOptions = {
+    particles: {
+      "move": {
+        "enable": true,
+        "speed": 8,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      },
+      number: {
+        value: 50,
+        density: {
+          enable: true,
+          value_area: 800,
+        }
+      }
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+        params={particleOptions} />
+    <Navigation />
+    <Logo />
+    <Rank />
+    <ImageLinkForm />
+    {/*<FaceRecognition>} */}
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
+  
