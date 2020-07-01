14bd5b2b60f17dbc4304e25c8a5a9654d0506f1b
eugene3231 <47620989+eugene3231@users.noreply.github.com>
2020-07-01 18:56:01 +0800

    fixing signin form to div

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -39,7 +39,7 @@ class SignIn extends React.Component {
         return (
             <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
-                    <form className="measure">
+                    <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                             <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                             <div className="mt3">
@@ -72,7 +72,7 @@ class SignIn extends React.Component {
                         onClick={() => onRouteChange('register')} 
                         href="#0" className="f6 link dim black db pointer">Register here!</p>
                     </div>
-                    </form>
+                    </div>
                 </main>
             </article>
             );
